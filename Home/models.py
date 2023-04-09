626a6cf6e3ecd782ab891f3696de91fc8eba5c9a
harsh <harsh.official995@gmail.com>
2023-04-09 20:11:39 +0530

    contact page

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    phone_no = PhoneNumberField(max_length=10)
+    message = models.TextField(max_length=200)
+    timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,HttpResponse
-
+from  .models import Contact
 # Create your views here.
 def home(request):
     # return HttpResponse("harsh is here")
     return render(request,'Home/home.html')
 
 def contact(request):
+    if request.method == 'POST':
+        first_name = str(request.POST['firstname'])
+        last_name = str(request.POST['lastname'])
+        email = str(request.POST['email'])
+        phone_no = str(request.POST['phone'])
+        message = str(request.POST['message'])
+        contact = Contact(first_name=first_name, last_name=last_name, email=email, phone_no=phone_no, message=message)
+        contact.save()
     return render(request,'Home/contact.html')
